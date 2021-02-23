6101440b901560b3efe87b76881e7ceee511081f
K.H.Park <svclaw2000@gmail.com>
2021-02-23 20:32:44 +0900

    Remake models

--- study/models.py
+++ study/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
 class Student(models.Model):
     username = models.CharField(max_length=31, unique=True)
     password = models.CharField(max_length=256)
     fullname = models.CharField(max_length=254)
     email = models.CharField(max_length=254)
     birthday = models.DateField()
+    level = models.IntegerField()
+    image_id = models.IntegerField()
+
 
 class Teacher(models.Model):
     username = models.CharField(max_length=31, unique=True)
@@ -14,7 +17,29 @@ class Teacher(models.Model):
     fullname = models.CharField(max_length=254)
     email = models.CharField(max_length=254)
     birthday = models.DateField()
+    account = models.CharField(max_length=254)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
 
 class Content(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
     content = models.TextField()
+    level = models.IntegerField()
+
+
+class Curriculum(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    content = models.ForeignKey('Content', on_delete=models.CASCADE)
+    percentage = models.IntegerField()
+    score = models.IntegerField(null=True)
+
+
+class DownloadHistory(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    content = models.ForeignKey('Content', on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
