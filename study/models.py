23f2caf7a2189ca111596b494d09df429ab7526e
K.H.Park <svclaw2000@gmail.com>
2021-02-23 20:24:10 +0900

    Remake models

--- study/models.py
+++ study/models.py
@@ -7,16 +7,39 @@ class Student(models.Model):
     fullname = models.CharField(max_length=254)
     email = models.CharField(max_length=254)
     birthday = models.DateField()
+    level = models.IntegerField()
+    image_id = models.IntegerField()
 
 
-class  Teacher(models.Model):
+class Teacher(models.Model):
     username = models.CharField(max_length=31, unique=True)
     password = models.CharField(max_length=256)
     fullname = models.CharField(max_length=254)
     email = models.CharField(max_length=254)
     birthday = models.DateField()
+    account = models.CharField(max_length=254)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
 
 
 class Content(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
     content = models.TextField()
+    level = models.IntegerField()
+
+
+class Curriculum(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    content = models.ForeignKey('Content', on_delete=models.CASCADE)
+    percentage = models.IntegerField()
+    score = models.IntegerField(null=True)
+
+
+class DownloadHistory(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    content = models.ForeignKey('Content', on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
